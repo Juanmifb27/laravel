5d371574172061634c2c41b43ec42a6154578ff4
juanmifb27-admin <juanmanuel.ferrera-berlanga@iesruizgijon.com>
2025-02-20 22:14:47 +0100

    Agregado los nombres de las rutas

--- Proyecto001/routes/web.php
+++ Proyecto001/routes/web.php
@@ -8,8 +8,8 @@ use App\Http\Controllers\CursoController;
 //     return view('welcome');
 // });
 
-Route::get('/', homeController::class);
+Route::get('/', homeController::class)->name('inicio');
 
-Route::get('cursos', [CursoController::class, 'index']);
-Route::get('cursos/create', [CursoController::class, 'create']);
-Route::get('cursos/{curso}', [CursoController::class, 'show']);
+Route::get('cursos', [CursoController::class, 'index'])->name('cursos');
+Route::get('cursos/create', [CursoController::class, 'create'])->name('crearCurso');
+Route::get('cursos/{curso}', [CursoController::class, 'show'])->name("mostrarCurso");
